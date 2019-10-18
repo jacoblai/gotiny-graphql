092b6e4eca12b015dd923eac91dffd99113847f6
jac <5241871@qq.com>
2019-10-18 11:30:52 +0800

    事务 fix

--- src/engine/PersonController.go
+++ src/engine/PersonController.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"context"
-	"errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -31,32 +30,26 @@ func (d *DbEngine) Search(ctx context.Context, args struct{ Name string }) ([]*m
 func (d *DbEngine) CreatePerson(ctx context.Context, args struct{ Input *models.InputPerson }) (*string, error) {
 	id := ""
 
-	sess, err := d.MgEngine.StartSession()
-	if err != nil {
-		return nil, errors.New("事务开始失败")
-	}
-	defer sess.EndSession(context.Background())
+	c := d.GetColl(models.T_Person)
 
-	err = d.MgEngine.UseSessionWithOptions(context.Background(), options.Session().SetDefaultReadPreference(readpref.Primary()), func(sessionContext mongo.SessionContext) error {
+	p := models.Person{
+		Name:           args.Input.Name,
+		Role:           args.Input.Role,
+		Address:        args.Input.Address,
+		Email:          args.Input.Email,
+		Phone:          args.Input.Phone,
+		Total:          args.Input.Total,
+		Order:          args.Input.Order,
+		CreatedAtFiled: time.Now().Local(),
+	}
+
+	err := d.MgEngine.UseSessionWithOptions(context.Background(), options.Session().SetDefaultReadPreference(readpref.Primary()), func(sessionContext mongo.SessionContext) error {
 		err := sessionContext.StartTransaction()
 		if err != nil {
 			return err
 		}
 
-		c := d.GetColl(models.T_Person)
-
-		p := models.Person{
-			Name:           args.Input.Name,
-			Role:           args.Input.Role,
-			Address:        args.Input.Address,
-			Email:          args.Input.Email,
-			Phone:          args.Input.Phone,
-			Total:          args.Input.Total,
-			Order:          args.Input.Order,
-			CreatedAtFiled: time.Now().Local(),
-		}
-
-		dbRes, err := c.InsertOne(context.Background(), &p)
+		dbRes, err := c.InsertOne(sessionContext, &p)
 		if err != nil {
 			_ = sessionContext.AbortTransaction(sessionContext)
 			return err
